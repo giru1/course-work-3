b8cf7af361951739d2d82ba65fd3b12e8025ee93
Giru <yagofarov.vadim@mail.ru>
2022-05-16 13:51:26 +0500

    fix small

--- project/views/auth.py
+++ project/views/auth.py
@@ -1,16 +1,19 @@
 from flask import request
 from flask_restx import Resource, Namespace
-
+# from project.dao.models.user import UserSchema
+# from project.implemented import user_service
 auth_ns = Namespace('auth')
 
 
-@auth_ns.route('/auth/register')
+@auth_ns.route('/register')
 class AuthRegisterView(Resource):
     def post(self):
         print(request.json)
+        # user_service.register(request.json)
+        # return {}, 201
 
 
-@auth_ns.route('/auth/login')
+@auth_ns.route('/login')
 class AuthLoginView(Resource):
     def post(self):
         pass
